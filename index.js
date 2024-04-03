8f070e7ab8e082c606ececf187194c0ac744ddaf
Allan <allanmuriithi441@gmail.com>
2024-04-03 05:37:51 -0400

    wise

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const body = document.querySelector('body')
+body.removeChild(document.querySelector('#main'));
+
+const newHeader = document.createElement('h1');
+newHeader.id  = 'victory';
+newHeader.textContent = 'Ombiji is the champion';
+body.appendChild(newHeader);
